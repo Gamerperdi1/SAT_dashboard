0b2cc942b66ced727a690a9a3a760c8d9907919a
Gamerperdi1 <Pedropala41@gmail.com>
2026-07-18 21:12:34 -0500

    Update humedad.js

--- humedad.js
+++ humedad.js
@@ -5,44 +5,75 @@ fetch("http://10.247.219.68:5000/datos")
     console.log("Datos recibidos:");
     console.log(datos);
 
+    // Tomar el valor de humedad del JSON
+    actualizarGrafico(datos.humedad);
+
+})
+.catch(error => {
+    console.error("Error al conectar con Flask:", error);
 });
-//configuracion del sensor//
+
+
+// configuración del sensor
 const sensor = {
     nombre : "Humedad del suelo",
     unidad : "%",
-    max : 50 //valor a modificar
+    max : 50
 };
+
 document.getElementById("tituloSensor").innerHTML = sensor.nombre;
+
+
 const total = 472;
+
+
 function actualizarGrafico(valor){
+
     const circle = document.getElementById("progressCircle");
     const number = document.getElementById("number");
-     if(!circle || !number){
+
+    if(!circle || !number){
         console.error("No se encontraron los elementos.");
         return;
     }
+
+
     valor = Number(valor);
-    if (isNaN(valor))
+
+    if(isNaN(valor))
         valor = 0;
-    number.innerHTML = valor + " "+sensor.unidad;
+
+
+    number.innerHTML = valor + " " + sensor.unidad;
+
+
     let porcentaje = (valor/sensor.max)*100;
-    if(porcentaje <0)
+
+
+    if(porcentaje < 0)
         porcentaje = 0;
-    if(porcentaje>100)
+
+    if(porcentaje > 100)
         porcentaje = 100;
-    let offset =total-(porcentaje/100)*total;
+
+
+    let offset = total-(porcentaje/100)*total;
+
+
     circle.style.strokeDashoffset = offset;
-    if(porcentaje<30){
+
+
+    if(porcentaje < 30){
         circle.style.stroke="#2ecc71";
     }
-    else if(porcentaje<60){
+    else if(porcentaje < 60){
         circle.style.stroke="#f1c40f";
     }
-    else if(porcentaje<80){
+    else if(porcentaje < 80){
         circle.style.stroke="#e67e22";
     }
     else{
         circle.style.stroke="#e74c3c";
     }
+
 }
-actualizarGrafico(0);
