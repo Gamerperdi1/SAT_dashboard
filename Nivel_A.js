549c66c1bbdcb99538c27227dbfa341d9e3c273b
Gamerperdi1 <Pedropala41@gmail.com>
2026-07-18 21:36:53 -0500

    Update Nivel_A.js

--- Nivel_A.js
+++ Nivel_A.js
@@ -1,40 +1,137 @@
-//configuracion del sensor//
+// ===============================
+// CONFIGURACIÓN DEL SENSOR
+// ===============================
+
 const sensor = {
     nombre : "Nivel del rio",
     unidad : "cm",
-    max : 40 //valor a modificar
+    max : 40
 };
+
+
 document.getElementById("tituloSensor").innerHTML = sensor.nombre;
+
+
 const total = 472;
+
+
+// ===============================
+// LECTURA DE DATOS DESDE FLASK
+// ===============================
+
+function leerDatos(){
+
+    fetch("http://10.247.219.68:5000/datos")
+
+    .then(response => response.json())
+
+    .then(datos => {
+
+        console.log("Nivel recibido:", datos.nivel);
+
+        actualizarGrafico(datos.nivel);
+
+    })
+
+    .catch(error => {
+
+        console.error("Error conectando con Flask:", error);
+
+    });
+
+}
+
+
+
+// ===============================
+// ACTUALIZAR GRÁFICA
+// ===============================
+
 function actualizarGrafico(valor){
+
     const circle = document.getElementById("progressCircle");
     const number = document.getElementById("number");
-     if(!circle || !number){
+
+
+    if(!circle || !number){
+
         console.error("No se encontraron los elementos.");
+
         return;
+
     }
+
+
     valor = Number(valor);
-    if (isNaN(valor))
+
+
+    if(isNaN(valor)){
+
         valor = 0;
-    number.innerHTML = valor + " "+sensor.unidad;
-    let porcentaje = (valor/sensor.max)*100;
-    if(porcentaje <0)
+
+    }
+
+
+    number.innerHTML = valor + " " + sensor.unidad;
+
+
+
+    let porcentaje = (valor / sensor.max) * 100;
+
+
+    if(porcentaje < 0){
+
         porcentaje = 0;
-    if(porcentaje>100)
+
+    }
+
+
+    if(porcentaje > 100){
+
         porcentaje = 100;
-    let offset =total-(porcentaje/100)*total;
+
+    }
+
+
+
+    let offset = total - (porcentaje / 100) * total;
+
+
     circle.style.strokeDashoffset = offset;
-    if(porcentaje<30){
+
+
+
+    if(porcentaje < 30){
+
         circle.style.stroke="#2ecc71";
+
     }
-    else if(porcentaje<60){
+
+    else if(porcentaje < 60){
+
         circle.style.stroke="#f1c40f";
+
     }
-    else if(porcentaje<80){
+
+    else if(porcentaje < 80){
+
         circle.style.stroke="#e67e22";
+
     }
+
     else{
+
         circle.style.stroke="#e74c3c";
+
     }
+
 }
-actualizarGrafico(0);
+
+
+// ===============================
+// ACTUALIZACIÓN AUTOMÁTICA
+// ===============================
+
+leerDatos();
+
+setInterval(leerDatos,2000);
